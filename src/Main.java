d6f12f57c62a04dc9a7915dee861d87e476dbed8
Igor Volnukhin <zybu@live.ru>
2020-04-08 17:45:32 +0300

    Ready Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int balance = 100;
+        int incoming = 124; // здесь вводить сумму
+        int bonus;
+        if (incoming > 1000) {
+            bonus = incoming / 100;
+        } else {
+            bonus = 0;
+        }
+        balance += incoming + bonus;
+        //System.out.printf("Bonus = " + bonus); // для тестирования
+        System.out.printf("Total balance = " + balance);
     }
 }
